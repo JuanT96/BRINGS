db60c6f57a33d704814b0b94688490a162a30294
Juan Manuel <juancho.triana@hotmail.com>
2025-07-05 20:57:49 -0500

    actualizacion barra de navegacion siempre visible

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
-let prevScrollPos = window.pageYOffset;
-const navbar = document.getElementById("navbar");
-const loginIcon = document.getElementById("loginIcon");
-const logoEst = document.getElementById("logoEst");
+//usar en caso de querer usar logica de desaparecer barra nav al bajar
 
-window.addEventListener("scroll", () => {
-  const currentScrollPos = window.pageYOffset;
+// let prevScrollPos = window.pageYOffset;
+// const navbar = document.getElementById("navbar");
+// const loginIcon = document.getElementById("loginIcon");
+// const logoEst = document.getElementById("logoEst");
 
-  if (prevScrollPos > currentScrollPos) {
-    // Scroll hacia arriba: mostrar barra y elementos
-    navbar.style.top = "20px";
-    loginIcon.style.top = "20px";
+// window.addEventListener("scroll", () => {
+//   const currentScrollPos = window.pageYOffset;
 
-    // El logo debe alinearse con la barra en ambas vistas
-    if (window.innerWidth < 960) {
-      logoEst.style.top = "70px"; // Ajuste móvil: debajo de la barra
-    } else {
-      logoEst.style.top = "0px"; // Escritorio: al mismo nivel
-    }
-  } else {
-    // Scroll hacia abajo: ocultar barra y elementos
-    navbar.style.top = "-100px";
-    loginIcon.style.top = "-100px";
-    logoEst.style.top = "-100px"; // Oculta el logo también
-  }
+//   if (prevScrollPos > currentScrollPos) {
+//     // Scroll hacia arriba: mostrar barra y elementos
+//     navbar.style.top = "20px";
+//     loginIcon.style.top = "20px";
 
-  prevScrollPos = currentScrollPos;
-});
+//     // El logo debe alinearse con la barra en ambas vistas
+//     if (window.innerWidth < 960) {
+//       logoEst.style.top = "70px"; // Ajuste móvil: debajo de la barra
+//     } else {
+//       logoEst.style.top = "0px"; // Escritorio: al mismo nivel
+//     }
+//   } else {
+//     // Scroll hacia abajo: ocultar barra y elementos
+//     navbar.style.top = "-100px";
+//     loginIcon.style.top = "-100px";
+//     logoEst.style.top = "-100px"; // Oculta el logo también
+//   }
+
+//   prevScrollPos = currentScrollPos;
+// });
 //slider de imagenes
 document.addEventListener("DOMContentLoaded", function () {
     const slides = document.querySelectorAll(".slider-seccion2 .slide");
